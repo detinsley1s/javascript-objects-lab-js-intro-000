192ea607f6a05cf58de0a0842ffe970375e9d218
funny-teacher-0837 <detinsley1s@gmail.com>
2020-03-27 05:21:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ function updateObjectWithKeyAndValue(obj, key, value) {
 function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
   return Object.assign(obj, {[key]: value})
 }
+
+function deleteFromObjectByKey(obj, key) {
+  let newObj = Object.assign({}, obj)
+  delete newObj[key]
+  return newObj
+}
