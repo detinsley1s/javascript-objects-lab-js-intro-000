051163eb93ab2d48d652d3608339b1b6f6fce650
funny-teacher-0837 <detinsley1s@gmail.com>
2020-03-27 05:16:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function updateObjectWithKeyAndValue(obj, key, value) {
-  let newObj = Object.assign({}, obj, {key: value})
+  let newObj = Object.assign({}, obj, {[key]: value})
   return newObj
 }
